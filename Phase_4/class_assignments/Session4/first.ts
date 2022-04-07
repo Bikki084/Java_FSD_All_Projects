c5e631af7ba9c4185fd81ac6fbf1c088ae085985
unknown <bikkishaw084@gmail.com>
2022-04-07 23:44:23 +0530

    class assignments added

---
+++ Phase_4/class_assignments/Session4/first.ts
@@ -0,0 +1,13 @@
+let a:number;
+a=56;
+let b:number=12;
+let fname:string="Alexa";
+console.log(a+b);
+console.log(fname);
+
+function test(num1:number, num2:number):number
+{
+    return num1+num2;
+}
+
+console.log(test(4, 6));
