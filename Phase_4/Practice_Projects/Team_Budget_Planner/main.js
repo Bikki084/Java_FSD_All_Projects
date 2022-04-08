749ddbb962ec02963e67b9882f6a3ee60c2e3d06
unknown <bikkishaw084@gmail.com>
2022-04-08 22:43:16 +0530

    added

--- Phase_4/Practice_Projects/Team_Budget_Planner/main.js
+++ Phase_4/Practice_Projects/Team_Budget_Planner/main.js
@@ -5,7 +5,6 @@ btn.onclick = function () {
   expName.value = "";
   expNumber.value = "";
   expenseForm.style.display = "block";
-  editForm.style.display = "none";
   modal.style.display = "block";
 };
 span.onclick = function () {
@@ -22,10 +21,7 @@ const addForm = document.getElementById("addForm");
 const budgetAmount = document.getElementById("budgetAmount");
 const balanceAmount = document.getElementById("balanceAmount");
 
-const editForm = document.getElementById("editForm");
-const saveEdit = document.getElementById("saveEdit");
-const editExpValue = document.getElementById("editExpValue");
-const editExpNumber = document.getElementById("editExpNumber");
+
 
 const expForm = document.getElementById("expForm");
 const expensesAmount = document.getElementById("expensesAmount");
@@ -53,7 +49,6 @@ function getBudgetAmount(amount) {
     balanceAmount.innerText = amount;
     expenseForm.style.display = "block";
     budgetform.style.display = "none";
-    editForm.style.display = "none";
     amountInput.value = "";
   }
 }
@@ -97,11 +92,10 @@ function displayExp(details) {
     expValue.innerHTML += `
     <div class="expValue" id="${details[i].id}">
       <div id="expTitleName" class="exp"><p>${details[i].name}</p></div>
-      <div id="expValueAmount" class="exp"><p> <span>$ </span> ${details[i].number}</p></div>
+      <div id="expValueAmount" class="exp"><p> <span>Rs.</span> ${details[i].number}</p></div>
       <div id="edite_delete">
         <p>
-          <button id="${details[i].id}" onclick="editExpDetails(${details[i].id})"> <img src="image/edit.svg" width="15" alt=""  /></button> 
-          <button id="${details[i].id}" onclick="delExpenseDetails(${details[i].id})"><img src="image/trash.svg" width="15" alt="" /></button>
+          <button id="${details[i].id}" onclick="delExpenseDetails(${details[i].id})"></button>
         </p>
       </div>
     </div>
@@ -131,36 +125,14 @@ function delExpenseDetails(id) {
   displayExp(details);
 }
 
-function editExpDetails(id) {
-  expenseForm.style.display = "none";
-  budgetform.style.display = "none";
-  editForm.style.display = "block";
-  details.findIndex((item) => {
-    if (item.id === id) {
-      editExpName.value = item.name;
-      editExpNumber.value = item.number;
-      saveEdit.children[2].id = item.id;
-      modal.style.display = "block";
-    }
-  });
-}
 
-function getExpValue(editExpName, editExpNumber, id) {
-  edited = details.findIndex((obj) => obj.id == id);
-  details[edited].name = editExpName;
-  details[edited].number = parseInt(editExpNumber);
-  displayExp(details);
-}
 
 function callBudget() {
   budgetform.style.display = "block";
   expenseForm.style.display = "none";
 }
 
-saveEdit.addEventListener("submit", (e) => {
-  e.preventDefault();
-  getExpValue(editExpName.value, editExpNumber.value, saveEdit.children[2].id);
-});
+
 
 expForm.addEventListener("submit", (e) => {
   e.preventDefault();
